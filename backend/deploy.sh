b78a3d2fb31966e581e999b3aaadf4f5ae24b695
Kuba Rożek <kubarozek@google.com>
2024-10-03 16:26:56 +0000

    backend works, deployment works

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -8,63 +8,65 @@ gcloud services enable cloudresourcemanager.googleapis.com
 gcloud auth application-default set-quota-project $GCP_PROJECT_ID
 printf "\nINFO - GCP project set to '$GCP_PROJECT_ID' succesfully!\n"
 
-# BUCKET_EXISTS=$(gcloud storage ls gs://$GCS_BUCKET > /dev/null 2>&1 && echo "true" || echo "false")
-# if "${BUCKET_EXISTS}"; then
-#   printf "\nWARN - Bucket '$GCS_BUCKET' already exists. Skipping bucket creation...\n"
-# else
-#   gcloud storage buckets create gs://$GCS_BUCKET --project=$GCP_PROJECT_ID --location=$GCP_REGION --uniform-bucket-level-access
-#   test $? -eq 0 || exit
-#   printf "\nINFO - Bucket '$GCS_BUCKET' created successfully in location '$GCP_REGION'!\n"
-# fi
+BUCKET_EXISTS=$(gcloud storage ls gs://$GCS_BUCKET > /dev/null 2>&1 && echo "true" || echo "false")
+if "${BUCKET_EXISTS}"; then
+  printf "\nWARN - Bucket '$GCS_BUCKET' already exists. Skipping bucket creation...\n"
+else
+  gcloud storage buckets create gs://$GCS_BUCKET --project=$GCP_PROJECT_ID --location=$GCP_REGION --uniform-bucket-level-access
+  test $? -eq 0 || exit
+  printf "\nINFO - Bucket '$GCS_BUCKET' created successfully in location '$GCP_REGION'!\n"
+fi
 
-# printf "\nINFO - Enabling GCP APIs...\n"
-# gcloud services enable \
-#   artifactregistry.googleapis.com \
-#   cloudbuild.googleapis.com \
-#   compute.googleapis.com \
-#   eventarc.googleapis.com \
-#   logging.googleapis.com \
-#   pubsub.googleapis.com \
-#   run.googleapis.com \
-#   script.googleapis.com \
-#   serviceusage.googleapis.com \
-#   storage.googleapis.com
+printf "\nINFO - Enabling GCP APIs...\n"
+gcloud services enable \
+  artifactregistry.googleapis.com \
+  cloudbuild.googleapis.com \
+  compute.googleapis.com \
+  eventarc.googleapis.com \
+  logging.googleapis.com \
+  pubsub.googleapis.com \
+  run.googleapis.com \
+  script.googleapis.com \
+  serviceusage.googleapis.com \
+  storage.googleapis.com
 
-# PROJECT_NUMBER=$(gcloud projects describe $GCP_PROJECT_ID --format="value(projectNumber)")
-# STORAGE_SERVICE_ACCOUNT="service-${PROJECT_NUMBER}@gs-project-accounts.iam.gserviceaccount.com"
-# EVENTARC_SERVICE_ACCOUNT="service-${PROJECT_NUMBER}@gcp-sa-eventarc.iam.gserviceaccount.com"
-# COMPUTE_SERVICE_ACCOUNT="${PROJECT_NUMBER}-compute@developer.gserviceaccount.com"
-# printf "\nINFO - Creating Service Agents and granting roles...\n"
-# for SA in "aiplatform.googleapis.com" "storage.googleapis.com" "eventarc.googleapis.com"; do
-#     gcloud --no-user-output-enabled beta services identity create --project=$GCP_PROJECT_ID \
-#         --service="${SA}"
-# done
-# COMPUTE_SA_ROLES=(
-#     "roles/eventarc.eventReceiver"
-#     "roles/run.invoker"
-#     "roles/cloudfunctions.invoker"
-#     "roles/storage.objectAdmin"
-#     "roles/aiplatform.user"
-#     "roles/logging.logWriter"
-#     "roles/artifactregistry.createOnPushWriter"
-#     "roles/cloudbuild.builds.builder"
-# )
-# for COMPUTE_SA_ROLE in "${COMPUTE_SA_ROLES[@]}"; do
-#     gcloud --no-user-output-enabled projects add-iam-policy-binding \
-#         $GCP_PROJECT_ID \
-#         --member="serviceAccount:${COMPUTE_SERVICE_ACCOUNT}" \
-#         --role="${COMPUTE_SA_ROLE}"
-# done
-# gcloud --no-user-output-enabled projects add-iam-policy-binding \
-#     $GCP_PROJECT_ID \
-#     --member="serviceAccount:${STORAGE_SERVICE_ACCOUNT}" \
-#     --role="roles/pubsub.publisher"
-# gcloud --no-user-output-enabled projects add-iam-policy-binding \
-#     $GCP_PROJECT_ID \
-#     --member="serviceAccount:${EVENTARC_SERVICE_ACCOUNT}" \
-#     --role="roles/eventarc.serviceAgent"
-# printf "Operation finished successfully!\n"
+PROJECT_NUMBER=$(gcloud projects describe $GCP_PROJECT_ID --format="value(projectNumber)")
+STORAGE_SERVICE_ACCOUNT="service-${PROJECT_NUMBER}@gs-project-accounts.iam.gserviceaccount.com"
+EVENTARC_SERVICE_ACCOUNT="service-${PROJECT_NUMBER}@gcp-sa-eventarc.iam.gserviceaccount.com"
+COMPUTE_SERVICE_ACCOUNT="${PROJECT_NUMBER}-compute@developer.gserviceaccount.com"
+printf "\nINFO - Creating Service Agents and granting roles...\n"
+for SA in "storage.googleapis.com" "eventarc.googleapis.com"; do
+    gcloud --no-user-output-enabled beta services identity create --project=$GCP_PROJECT_ID \
+        --service="${SA}"
+done
+COMPUTE_SA_ROLES=(
+    "roles/eventarc.eventReceiver"
+    "roles/run.invoker"
+    "roles/cloudfunctions.invoker"
+    "roles/storage.objectAdmin"
+    "roles/aiplatform.user"
+    "roles/logging.logWriter"
+    "roles/artifactregistry.createOnPushWriter"
+    "roles/cloudbuild.builds.builder"
+    "roles/run.invoker"
+)
+for COMPUTE_SA_ROLE in "${COMPUTE_SA_ROLES[@]}"; do
+    gcloud --no-user-output-enabled projects add-iam-policy-binding \
+        $GCP_PROJECT_ID \
+        --member="serviceAccount:${COMPUTE_SERVICE_ACCOUNT}" \
+        --role="${COMPUTE_SA_ROLE}"
+done
+gcloud --no-user-output-enabled projects add-iam-policy-binding \
+    $GCP_PROJECT_ID \
+    --member="serviceAccount:${STORAGE_SERVICE_ACCOUNT}" \
+    --role="roles/pubsub.publisher"
+gcloud --no-user-output-enabled projects add-iam-policy-binding \
+    $GCP_PROJECT_ID \
+    --member="serviceAccount:${EVENTARC_SERVICE_ACCOUNT}" \
+    --role="roles/eventarc.serviceAgent"
+printf "Operation finished successfully!\n"
 
+printf "\nSetting up Docker registry and pushing image into it"
 DOCKER_REPO_NAME=gps-docker-repo
 REPO_EXISTS=$(gcloud artifacts repositories describe $DOCKER_REPO_NAME --location=$GCP_REGION > /dev/null 2>&1 && echo "true" || echo "false")
 if "${REPO_EXISTS}"; then
@@ -79,9 +81,9 @@ else
 fi
 
 ARTIFACT_POSITORY_NAME=$GCP_REGION-docker.pkg.dev/$GCP_PROJECT_ID/$DOCKER_REPO_NAME
+DOCKER_IMAGE_TAG=$ARTIFACT_POSITORY_NAME/ariel-process:latest
 
 printf "\nINFO Building Docker image for ariel processor"
-DOCKER_IMAGE_TAG=$ARTIFACT_POSITORY_NAME/ariel-process:latest
 docker build -t $DOCKER_IMAGE_TAG .
 docker push $DOCKER_IMAGE_TAG
 
@@ -96,14 +98,14 @@ gcloud beta run deploy ariel-process \
   --gpu-type=nvidia-l4 \
   --max-instances=1 \
   --timeout=600s \
-  --concurrency=1
+  --concurrency=1 \
+  --set-env-vars PROJECT_ID=$GCP_PROJECT_ID
 
 printf "\nINFO Setting up triggers from GCS to Ariel processor topic in Cloud Run"
 gcloud eventarc triggers create ariel-bucket-trigger \
   --destination-run-service=ariel-process \
   --destination-run-region=$GCP_REGION \
   --event-filters="type=google.cloud.storage.object.v1.finalized" \
-  --event-filters="serviceName=storage.googleapis.com" \
   --event-filters="bucket=$GCS_BUCKET" \
   --location="$GCP_REGION" \
   --service-account="$COMPUTE_SERVICE_ACCOUNT"
@@ -115,10 +117,8 @@ SERVICE_URL=$(gcloud run services describe ariel-process --region $GCP_REGION --
 
 gcloud pubsub subscriptions create ariel-process-subscription --topic ariel-topic \
   --ack-deadline=600 \
-  --push-endpoint=$SERVICE_URL/
+  --push-endpoint=$SERVICE_URL/ \
   --push-auth-service-account="$COMPUTE_SERVICE_ACCOUNT"
-
-# gcloud eventarc triggers create my-trigger --event-filters="type=google.cloud.pubsub.topic.v1.messagePublished" --destination-run-service=my-service
 
 test $? -eq 0 || exit
 
