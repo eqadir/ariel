f575e3e15636038faeda06043b0f74e313aaddff
Kuba Rożek <kubarozek@google.com>
2024-10-03 14:09:11 +0000

    second commit - cloud is still weird

--- backend/deploy_function_only.sh
+++ backend/deploy_function_only.sh
@@ -7,19 +7,7 @@ GCS_BUCKET=cse-kubarozek-sandbox-ariel-us
 PROJECT_NUMBER=$(gcloud projects describe $GCP_PROJECT_ID --format="value(projectNumber)")
 COMPUTE_SERVICE_ACCOUNT="${PROJECT_NUMBER}-compute@developer.gserviceaccount.com"
 
-#build on cloud build
-# gcloud beta run deploy ariel-process \
-#   --region=$GCP_REGION \
-#   --no-allow-unauthenticated \
-#   --source=. \
-#   --memory=32Gi \
-#   --cpu=8 \
-#   --gpu=1 \
-#   --gpu-type=nvidia-l4 \
-#   --max-instances=1 \
-#   --timeout=600s
-
-#ALT: build locally, push to artifact registry and deploy from there (As opposed to cloud build, maybe it'll be faster)
+#build locally, push to artifact registry and deploy from there (As opposed to cloud build, it'll be faster)
 DOCKER_REPO_NAME=gps-docker-repo
 echo **IGNORE the following error, if the repository already exists**
 gcloud artifacts repositories create $DOCKER_REPO_NAME --repository-format=docker \
@@ -40,13 +28,26 @@ gcloud beta run deploy ariel-process \
   --gpu=1 \
   --gpu-type=nvidia-l4 \
   --max-instances=1 \
-  --timeout=600s
+  --timeout=600s \
+  --concurrency=1
 
-#anyway: setup trigger
-# gcloud eventarc triggers create ariel-process-trigger \
-#   --destination-run-service=ariel-process \
-#   --destination-run-region=$GCP_REGION \
-#   --event-filters="bucket=$GCS_BUCKET" \
-#   --event-filters="type=google.cloud.storage.object.v1.finalized" \
-#   --location="$GCP_REGION" \
-#   --service-account="$COMPUTE_SERVICE_ACCOUNT"
+#setup triggers
+gcloud eventarc triggers create ariel-config-trigger \
+  --destination-run-service=ariel-process \
+  --destination-run-region=$GCP_REGION \
+  --event-filters="type=google.cloud.audit.log.v1.written" \
+  --event-filters="serviceName=storage.googleapis.com" \
+  --event-filters="methodName=storage.objects.create" \
+  --event-filters-path-pattern="resourceName=/projects/_/buckets/$GCS_BUCKET/**/config.json" \
+  --location="$GCP_REGION" \
+  --service-account="$COMPUTE_SERVICE_ACCOUNT"
+
+gcloud eventarc triggers create ariel-approved-trigger \
+  --destination-run-service=ariel-process \
+  --destination-run-region=$GCP_REGION \
+  --event-filters="type=google.cloud.audit.log.v1.written" \
+  --event-filters="serviceName=storage.googleapis.com" \
+  --event-filters="methodName=storage.objects.create" \
+  --event-filters-path-pattern="resourceName=/projects/_/buckets/$GCS_BUCKET/**/*_approved.json" \
+  --location="$GCP_REGION" \
+  --service-account="$COMPUTE_SERVICE_ACCOUNT"
